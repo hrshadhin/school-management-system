e77aeb1724c9d6c7685931614f7a1834ae455e17
H.R.Shadhin <hrshadhin.i386@gmail.com>
2018-05-21 00:18:34 +0600

    app basic setup done

---
+++ app/Http/Middleware/Language.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+
+class Language
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //set user wise locale
+        App::setLocale(session('user_locale', config('app.locale')));
+        
+        return $next($request);
+    }
+}
