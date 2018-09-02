ee5c37ea42e22fbdd98bd329d63eaaedadccfa88
H.R.Shadhin <hrshadhin.i386@gmail.com>
2018-09-03 00:44:25 +0600

    add todo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -33,7 +33,7 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
      * @return void
      */
     public function report(Exception $exception)
@@ -44,15 +44,15 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception               $exception
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
         if(!env('APP_DEBUG', false)) {
             $statusCode = $exception->getStatusCode();
-
+            // todo: need to fixed unauthenticate route error here
 
             if ($statusCode == 404) {
                 $locale = Session::get('user_locale');
