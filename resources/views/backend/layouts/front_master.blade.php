4b0a997a758b6b90a1c1d25dd75229fec2b5b426
H.R.Shadhin <hrshadhin.i386@gmail.com>
2018-09-15 22:43:50 +0600

    put protection on delete the resources

--- resources/views/backend/layouts/front_master.blade.php
+++ resources/views/backend/layouts/front_master.blade.php
@@ -29,9 +29,7 @@
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    <!-- Child Page css goes here  -->
+  <!-- Child Page css goes here  -->
     @yield("extraStyle")
     <!-- Child Page css -->	
 
