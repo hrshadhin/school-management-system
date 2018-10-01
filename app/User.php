87c2920df460df2b63bfb90f05bd8bf0a76136f9
H.R.Shadhin <shadhin@stsbd.com>
2018-10-01 17:15:52 +0600

    user module done

--- app/User.php
+++ app/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Employee');
     }
+
+    public function role()
+    {
+        return $this->hasOne('App\UserRole');
+    }
 }
