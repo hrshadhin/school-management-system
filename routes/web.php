555c6883adbbe8640c72d52aa7827873d78b84bc
H.R.Shadhin <hrshadhin.i386@gmail.com>
2018-11-20 18:47:19 +0600

    fixed route bug

--- routes/web.php
+++ routes/web.php
@@ -25,149 +25,150 @@ use App\Http\Helpers\AppHelper;
  */
 Route::group(
     ['namespace' => 'Backend', 'middleware' => ['guest']], function () {
-    Route::get('/login', 'UserController@login')->name('login');
-    Route::post('/login', 'UserController@authenticate');
-    Route::get('/forgot', 'UserController@forgot')->name('forgot');
-    Route::post('/forgot', 'UserController@forgot')
+        Route::get('/login', 'UserController@login')->name('login');
+        Route::post('/login', 'UserController@authenticate');
+        Route::get('/forgot', 'UserController@forgot')->name('forgot');
+        Route::post('/forgot', 'UserController@forgot')
         ->name('forgot');
-    Route::get('/reset/{token}', 'UserController@reset')
+        Route::get('/reset/{token}', 'UserController@reset')
         ->name('reset');
-    Route::post('/reset/{token}', 'UserController@reset')
+        Route::post('/reset/{token}', 'UserController@reset')
         ->name('reset');
 
-}
+    }
 );
 
 Route::group(
     ['namespace' => 'Backend', 'middleware' => ['auth']], function () {
-    Route::get('/logout', 'UserController@logout')->name('logout');
-    Route::get('/lock', 'UserController@lock')->name('lockscreen');
-    Route::get('/dashboard', 'UserController@dashboard')->name('user.dashboard');
+        Route::get('/logout', 'UserController@logout')->name('logout');
+        Route::get('/lock', 'UserController@lock')->name('lockscreen');
+        Route::get('/dashboard', 'UserController@dashboard')->name('user.dashboard');
 
-    //user management
-    Route::resource('user','UserController');
-    Route::get('/profile', 'UserController@profile')
+        //user management
+        Route::resource('user', 'UserController');
+        Route::get('/profile', 'UserController@profile')
         ->name('profile');
-    Route::post('/profile', 'UserController@profile')
+        Route::post('/profile', 'UserController@profile')
         ->name('profile');
-    Route::get('/change-password', 'UserController@changePassword')
+        Route::get('/change-password', 'UserController@changePassword')
         ->name('change_password');
-    Route::post('/change-password', 'UserController@changePassword')
+        Route::post('/change-password', 'UserController@changePassword')
         ->name('change_password');
-    Route::post('user/status/{id}','UserController@changeStatus')
+        Route::post('user/status/{id}', 'UserController@changeStatus')
         ->name('user.status');
-    Route::any('user/{id}/permission','UserController@updatePermission')
+        Route::any('user/{id}/permission', 'UserController@updatePermission')
         ->name('user.permission');
 
-    //user notification
-    Route::get('/notification/unread', 'NotificationController@getUnReadNotification')
+        //user notification
+        Route::get('/notification/unread', 'NotificationController@getUnReadNotification')
         ->name('user.notification_unread');
-    Route::get('/notification/read', 'NotificationController@getReadNotification')
+        Route::get('/notification/read', 'NotificationController@getReadNotification')
         ->name('user.notification_read');
-    Route::get('/notification/all', 'NotificationController@getAllNotification')
+        Route::get('/notification/all', 'NotificationController@getAllNotification')
         ->name('user.notification_all');
 
-    //system user management
-    Route::get('/administrator/user', 'AdministratorController@userIndex')
+        //system user management
+        Route::get('/administrator/user', 'AdministratorController@userIndex')
         ->name('administrator.user_index');
-    Route::get('/administrator/user/create', 'AdministratorController@userCreate')
+        Route::get('/administrator/user/create', 'AdministratorController@userCreate')
         ->name('administrator.user_create');
-    Route::post('/administrator/user/store', 'AdministratorController@userStore')
+        Route::post('/administrator/user/store', 'AdministratorController@userStore')
         ->name('administrator.user_store');
-    Route::get('/administrator/user/{id}/edit', 'AdministratorController@userEdit')
+        Route::get('/administrator/user/{id}/edit', 'AdministratorController@userEdit')
         ->name('administrator.user_edit');
-    Route::post('/administrator/user/{id}/update', 'AdministratorController@userUpdate')
+        Route::post('/administrator/user/{id}/update', 'AdministratorController@userUpdate')
         ->name('administrator.user_update');
-    Route::post('/administrator/user/{id}/delete', 'AdministratorController@userDestroy')
+        Route::post('/administrator/user/{id}/delete', 'AdministratorController@userDestroy')
         ->name('administrator.user_destroy');
-    Route::post('administrator/user/status/{id}','AdministratorController@userChangeStatus')
+        Route::post('administrator/user/status/{id}', 'AdministratorController@userChangeStatus')
         ->name('administrator.user_status');
 
-    Route::any('/administrator/user/reset-password', 'AdministratorController@userResetPassword')
+        Route::any('/administrator/user/reset-password', 'AdministratorController@userResetPassword')
         ->name('administrator.user_password_reset');
 
 
 
-    //user role manage
-    Route::get('/role', 'UserController@roles')
+        //user role manage
+        Route::get('/role', 'UserController@roles')
         ->name('user.role_index');
-    Route::post('/role', 'UserController@roles')
+        Route::post('/role', 'UserController@roles')
         ->name('user.role_destroy');
-    Route::get('/role/create', 'UserController@roleCreate')
+        Route::get('/role/create', 'UserController@roleCreate')
         ->name('user.role_create');
-    Route::post('/role/store', 'UserController@roleCreate')
+        Route::post('/role/store', 'UserController@roleCreate')
         ->name('user.role_store');
-    Route::any('/role/update/{id}', 'UserController@roleUpdate')
+        Route::any('/role/update/{id}', 'UserController@roleUpdate')
         ->name('user.role_update');
 
 
-    // application settings routes
-    Route::get('settings/institute','SettingsController@institute')
+        // application settings routes
+        Route::get('settings/institute', 'SettingsController@institute')
         ->name('settings.institute');
-    Route::post('settings/institute','SettingsController@institute')
+        Route::post('settings/institute', 'SettingsController@institute')
         ->name('settings.institute');
 
-    // administrator routes
+        // administrator routes
         //academic year
-    Route::get('administrator/academic_year','AdministratorController@academicYearIndex')
+        Route::get('administrator/academic_year', 'AdministratorController@academicYearIndex')
         ->name('administrator.academic_year');
-    Route::post('administrator/academic_year','AdministratorController@academicYearIndex')
+        Route::post('administrator/academic_year', 'AdministratorController@academicYearIndex')
         ->name('administrator.academic_year_destroy');
-    Route::get('administrator/academic_year/create','AdministratorController@academicYearCru')
+        Route::get('administrator/academic_year/create', 'AdministratorController@academicYearCru')
         ->name('administrator.academic_year_create');
-    Route::post('administrator/academic_year/create','AdministratorController@academicYearCru')
+        Route::post('administrator/academic_year/create', 'AdministratorController@academicYearCru')
         ->name('administrator.academic_year_store');
-    Route::get('administrator/academic_year/edit/{id}','AdministratorController@academicYearCru')
+        Route::get('administrator/academic_year/edit/{id}', 'AdministratorController@academicYearCru')
         ->name('administrator.academic_year_edit');
-    Route::post('administrator/academic_year/update/{id}','AdministratorController@academicYearCru')
+        Route::post('administrator/academic_year/update/{id}', 'AdministratorController@academicYearCru')
         ->name('administrator.academic_year_update');
-    Route::post('administrator/academic_year/status/{id}','AdministratorController@academicYearChangeStatus')
+        Route::post('administrator/academic_year/status/{id}', 'AdministratorController@academicYearChangeStatus')
         ->name('administrator.academic_year_status');
 
-    // academic routes
-    // class
-    Route::get('academic/class','AcademicController@classIndex')
+        // academic routes
+        // class
+        Route::get('academic/class', 'AcademicController@classIndex')
         ->name('academic.class');
-    Route::post('academic/class','AcademicController@classIndex')
+        Route::post('academic/class', 'AcademicController@classIndex')
         ->name('academic.class_destroy');
-    Route::get('academic/class/create','AcademicController@classCru')
+        Route::get('academic/class/create', 'AcademicController@classCru')
         ->name('academic.class_create');
-    Route::post('academic/class/create','AcademicController@classCru')
+        Route::post('academic/class/create', 'AcademicController@classCru')
         ->name('academic.class_store');
-    Route::get('academic/class/edit/{id}','AcademicController@classCru')
+        Route::get('academic/class/edit/{id}', 'AcademicController@classCru')
         ->name('academic.class_edit');
-    Route::post('academic/class/update/{id}','AcademicController@classCru')
+        Route::post('academic/class/update/{id}', 'AcademicController@classCru')
         ->name('academic.class_update');
-    Route::post('academic/class/status/{id}','AcademicController@classStatus')
+        Route::post('academic/class/status/{id}', 'AcademicController@classStatus')
         ->name('academic.class_status');
 
-    // section
-    Route::get('academic/section','AcademicController@sectionIndex')
+        // section
+        Route::get('academic/section', 'AcademicController@sectionIndex')
         ->name('academic.section');
-    Route::post('academic/section','AcademicController@sectionIndex')
+        Route::post('academic/section', 'AcademicController@sectionIndex')
         ->name('academic.section_destroy');
-    Route::get('academic/section/create','AcademicController@sectionCru')
+        Route::get('academic/section/create', 'AcademicController@sectionCru')
         ->name('academic.section_create');
-    Route::post('academic/section/create','AcademicController@sectionCru')
+        Route::post('academic/section/create', 'AcademicController@sectionCru')
         ->name('academic.section_store');
-    Route::get('academic/section/edit/{id}','AcademicController@sectionCru')
+        Route::get('academic/section/edit/{id}', 'AcademicController@sectionCru')
         ->name('academic.section_edit');
-    Route::post('academic/section/update/{id}','AcademicController@sectionCru')
+        Route::post('academic/section/update/{id}', 'AcademicController@sectionCru')
         ->name('academic.section_update');
-    Route::post('academic/section/status/{id}','AcademicController@sectionStatus')
+        Route::post('academic/section/status/{id}', 'AcademicController@sectionStatus')
         ->name('academic.section_status');
 
-    // teacher routes
-    Route::resource('teacher', 'TeacherController');
-    Route::post('teacher/status/{id}','TeacherController@changeStatus')
+        // teacher routes
+        Route::resource('teacher', 'TeacherController');
+        Route::post('teacher/status/{id}', 'TeacherController@changeStatus')
         ->name('teacher.status');
 
-    // student routes
-    Route::resource('student', 'StudentController');
-    Route::post('student/status/{id}','StudentController@changeStatus')
+        // student routes
+        Route::resource('student', 'StudentController');
+        Route::post('student/status/{id}', 'StudentController@changeStatus')
         ->name('student.status');
 
-});
+    }
+);
 
 //change website locale
 Route::get(
@@ -175,34 +176,41 @@ Route::get(
         //set user wanted locale to session
         Session::put('user_locale', $lang);
         return redirect()->back();
-})->name('setLocale');
+    }
+)->name('setLocale');
 
 
 //dev routes
-Route::get('/make-link',function($code){
-    if($code !== '007'){
-        return 'Wrong code!';
+Route::get(
+    '/make-link/{code}', function ($code) {
+        if($code !== '007') {
+            return 'Wrong code!';
+        }
+        App::make('files')->link(storage_path('app/public'), public_path('storage'));
+        return 'Done link';
     }
-    App::make('files')->link(storage_path('app/public'), public_path('storage'));
-    return 'Done link';
-});
-Route::get('/clear-cache', function($code) {
-    if($code !== '007'){
-        return 'Wrong code!';
+);
+Route::get(
+    '/clear-cache/{code}', function ($code) {
+        if($code !== '007') {
+            return 'Wrong code!';
+        }
+        $exitCode = Artisan::call('cache:clear');
+        $exitCode = Artisan::call('view:clear');
+        $exitCode = Artisan::call('route:clear');
+        return 'clear cache';
     }
-    $exitCode = Artisan::call('cache:clear');
-    $exitCode = Artisan::call('view:clear');
-    $exitCode = Artisan::call('route:clear');
-    return 'clear cache';
-});
+);
 
 // create tiggers
-Route::get('/create-triggers/{code}', function($code) {
-    if($code !== '007'){
-        return 'Wrong code!';
+Route::get(
+    '/create-triggers/{code}', function ($code) {
+        if($code !== '007') {
+            return 'Wrong code!';
+        }
+
+        AppHelper::createTriggers();
+
+        return 'Triggers created :)';
     }
-
-    AppHelper::createTriggers();
-
-    return 'Triggers created :)';
-});
+);
