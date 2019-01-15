3b42b681a398739782fe6e01a409fe142baa560b
H.R.Shadhin <shadhin@stsbd.com>
2019-01-15 17:06:31 +0600

    mail and sms template started

---
+++ database/migrations/2019_01_15_130708_create_templates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('type', [array_keys(\App\Http\Helpers\AppHelper::TEMPLATE_TYPE)]);
+            $table->unsignedInteger('role_id')->default(0);
+            $table->longText('content');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->userstamps();
+
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+}
