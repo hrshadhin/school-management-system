c2f6ff0d2a0db45520e4874c2f62b2c5dc73c43a
H.R.Shadhin <shadhin@stsbd.com>
2019-01-15 17:06:31 +0600

    mail and sms template started

---
+++ app/Template.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Helpers\AppHelper;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Hrshadhin\Userstamps\UserstampsTrait;
+use Illuminate\Support\Arr;
+
+class Template extends Model
+{
+    use SoftDeletes;
+    use UserstampsTrait;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'type',
+        'role_id',
+        'content',
+    ];
+
+
+
+    public function user()
+    {
+        return $this->belongsTo('App\Role');
+    }
+
+    public function getTypeAttribute($value)
+    {
+        return Arr::get(AppHelper::TEMPLATE_TYPE, $value);
+    }
+
+
+}
