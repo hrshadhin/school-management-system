a39219d0b3a10368603c7e1e51d2f34e15608aad
H.R.Shadhin <hrshadhin.i386@gmail.com>
2019-01-19 11:21:27 +0600

    attendance file upload start

---
+++ database/migrations/2019_01_19_092522_create_attendance_file_queues_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendanceFileQueuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_file_queues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('file_name');
+            $table->string('client_file_name');
+            $table->integer('file_format')->default(0);
+            $table->integer('total_rows');
+            $table->integer('imported_rows');
+            $table->smallInteger('is_imported')->default(0);
+            $table->smallInteger('send_sms')->default(0);
+            $table->smallInteger('is_sms_send')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->userstamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_file_queues');
+    }
+}
