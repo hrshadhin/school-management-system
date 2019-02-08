16ddb7bb3e424b670925ed559a63e700be2ee554
H.R.Shadhin <hrshadhin.i386@gmail.com>
2019-02-08 14:50:18 +0600

    employee module view added

---
+++ resources/views/backend/hrm/employee/list.blade.php
@@ -0,0 +1,131 @@
+<!-- Master page  -->
+@extends('backend.layouts.master')
+
+<!-- Page title -->
+@section('pageTitle') Employee @endsection
+<!-- End block -->
+
+<!-- Page body extra class -->
+@section('bodyCssClass') @endsection
+<!-- End block -->
+
+<!-- BEGIN PAGE CONTENT-->
+@section('pageContent')
+    <!-- Section header -->
+    <section class="content-header">
+        <h1>
+            Employee
+            <small>List</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{{URL::route('user.dashboard')}}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">Employee</li>
+        </ol>
+    </section>
+    <!-- ./Section header -->
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-info">
+                    <div class="box-header">
+                        <div class="box-tools pull-right">
+                            <a class="btn btn-info btn-sm" href="{{ URL::route('hrm.employee.create') }}"><i class="fa fa-plus-circle"></i> Add New</a>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body margin-top-20">
+                        <div class="table-responsive">
+                        <table id="listDataTableWithSearch" class="table table-bordered table-striped list_view_table display responsive no-wrap" width="100%">
+                            <thead>
+                            <tr>
+                                <th width="5%">#</th>
+                                <th class="notexport" width="10%">Photo</th>
+                                <th width="8%">ID Card</th>
+                                <th width="25%">Name</th>
+                                <th width="8%">Phone No</th>
+                                <th width="19%">Email</th>
+                                <th width="10%">Status</th>
+                                <th class="notexport" width="15%">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($employees as $employee)
+                                <tr>
+                                    <td>
+                                        {{$loop->iteration}}
+                                    </td>
+                                    <td>
+                                        <img class="img-responsive center" style="height: 35px; width: 35px;" src="@if($employee->photo ){{ asset('storage/employee')}}/{{ $employee->photo }} @else {{ asset('images/avatar.jpg')}} @endif" alt="">
+                                    </td>
+                                    <td>{{ $employee->id_card }}</td>
+                                    <td>{{ $employee->name }}</td>
+                                    <td>{{ $employee->phone_no }}</td>
+                                    <td>{{ $employee->email }}</td>
+                                    <td>
+                                        <!-- todo: have problem in mobile device -->
+                                        <input class="statusChange" type="checkbox" data-pk="{{$employee->id}}" @if($employee->status) checked @endif data-toggle="toggle" data-on="<i class='fa fa-check-circle'></i>" data-off="<i class='fa fa-ban'></i>" data-onstyle="success" data-offstyle="danger">
+                                    </td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <a title="Details"  href="{{URL::route('teacher.show',$employee->id)}}"  class="btn btn-primary btn-sm"><i class="fa fa-eye"></i>
+                                            </a>
+                                        </div>
+                                        <div class="btn-group">
+                                            <a title="Edit" href="{{URL::route('teacher.edit',$employee->id)}}" class="btn btn-info btn-sm"><i class="fa fa-edit"></i></a>
+                                            </a>
+                                        </div>
+                                        <!-- todo: have problem in mobile device -->
+                                        <div class="btn-group">
+                                            <form  class="myAction" method="POST" action="{{URL::route('teacher.destroy', $employee->id)}}">
+                                                @csrf
+                                                <input name="_method" type="hidden" value="DELETE">
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete">
+                                                    <i class="fa fa-fw fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </div>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th width="5%">#</th>
+                                <th class="notexport" width="10%">Photo</th>
+                                <th width="8%">ID Card</th>
+                                <th width="25%">Name</th>
+                                <th width="8%">Phone No</th>
+                                <th width="15%">Email</th>
+                                <th width="10%">Status</th>
+                                <th class="notexport" width="15%">Action</th>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- /.content -->
+@endsection
+<!-- END PAGE CONTENT-->
+
+<!-- BEGIN PAGE JS-->
+@section('extraScript')
+    <script type="text/javascript">
+        $(document).ready(function () {
+            window.postUrl = '{{URL::Route("hrm.employee.status", 0)}}';
+            window.changeExportColumnIndex = 5;
+            window.excludeFilterComlumns = [0,1,6,7];
+            Generic.initCommonPageJS();
+            Generic.initDeleteDialog();
+        });
+    </script>
+@endsection
+<!-- END PAGE JS-->
