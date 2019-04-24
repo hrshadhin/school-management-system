141d528c0e85c051c7561fea322634029179d173
H.R.Shadhin <hrshadhin.i386@gmail.com>
2019-04-24 11:16:48 +0600

    update depency and fixed bug

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Helpers\AppHelper;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,6 +19,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            if (Auth::viaRemember()) {
+                session(['user_session_sha1' => AppHelper::getUserSessionHash()]);
+                session(['user_role_id' => auth()->user()->role->role_id]);
+            }
             return redirect()->route('user.dashboard');
         }
 
