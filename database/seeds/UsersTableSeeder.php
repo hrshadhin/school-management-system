509e88c0b62aa5658bf9e7557fe27b0e0c1671a9
H.R.Shadhin <hrshadhin.i386@gmail.com>
2019-06-22 11:58:06 +0600

    update libs and laravel 5.8

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        echo PHP_EOL , 'seeding users...';
+        echo 'seeding users...', PHP_EOL;
 
         $user= User::create(
             [
