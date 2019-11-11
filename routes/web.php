18b241af9a7bf0458242a77cbf450e01ff40d612
mohammad-azim <mmadazms313@yahoo.com>
2019-11-11 05:17:07 +0430

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Session;
 use App\Http\Helpers\AppHelper;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 //Route::group(['middleware' => 'role:admin'], function() {
 //    Route::get('/admin', function() {
