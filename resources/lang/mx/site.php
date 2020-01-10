f621626f727e6dc5ede10a8d01989c1848488b35
Habibur Rahman Shadhin <hrshadhin.i386@gmail.com>
2020-01-10 17:21:44 +0600

    mexican translation added.

---
+++ resources/lang/mx/site.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Website Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during website frontend
+    */
+
+    'menu_home' => 'Home',
+    'menu_class' => 'Class',
+    'menu_teachers' => 'Teachers',
+    'menu_events' => 'Events',
+    'menu_gallery' => 'Gallery',
+    'menu_contact_us' => 'Contact Us',
+    'menu_faq' => 'FAQ',
+    'menu_admission' => 'Admission',
+    'menu_result' => 'Results',
+    'menu_timeline' => 'Timeline',
+    'empty_content' => 'Please add content from admin panel।',
+    'apply_now' => 'Apply Now',
+    'about_us' => 'About Us',
+    'why_we' => 'Why We Are Better',
+    'about_us_more' => 'Learn More About Us From Video',
+    'about_us_more2' => 'Watch More',
+    'service' => 'Our Services',
+    'stat_students' => 'Students Enrolled',
+    'stat_teachers' => 'Teachers',
+    'stat_college' => 'Passing to College',
+    'stat_books' => 'Books',
+    'testimonials' => 'Testimonials',
+    'get_in' => 'Get In Touch With Us',
+    'drop_email' => 'Drop your email here to get latest updates from us.',
+    'write_email' => 'Enter your email',
+    'subscribe' => 'Subscribe',
+    'up_event' => 'Upcoming Event',
+    'help_links' => 'Help Links',
+    'copy_right' => date('Y').' . All Rights Reserved',
+    'maintainer' => 'Maintained by',
+    'details' => 'Details',
+    'information' => 'Information',
+    'teacher' => 'Teacher',
+    'room_no' => 'Room No',
+    'capacity' => 'Capacity',
+    'shift' => 'Shift',
+    'description' => 'Description',
+    'course_outline' => 'Course Outline',
+    'meet_teacher' => 'Meet our Teachers',
+    'more' => 'More',
+    'gallery_title' => 'Our Memories',
+    'contact_us_form_title' => 'Contact Us',
+    'yn' => 'Your name',
+    'ye' => 'Your email',
+    'subject' => 'Subject',
+    'ym' => 'Your Message',
+    'send' => 'Send',
+    'address' => 'Address',
+    'phone_no' => 'Phone Number',
+    'email' => 'E-mail',
+    'our_office' => 'Our Office',
+    'faq_title' => 'Frequently asked questions',
+    'timeline_title' => 'Our Journey',
+    'not_found' => '404',
+    'not_found_title' => 'This page doesn\'t exist.',
+    'code_500' => '500',
+    'code_500_title' => 'Internal Server Error! Contact System Administrator.',
+
+
+
+];
